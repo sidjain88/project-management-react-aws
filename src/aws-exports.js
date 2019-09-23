106aedfc7b1c7e69894c6f5d0cad156394c90d14
Siddharth Jain (Oakton) <siddharth.jain@global.ntt>
2019-09-23 16:24:01 +1000

    added environment details

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,17 +2,17 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_appsync_graphqlEndpoint": "https://ougo52ihunawhlxdf2qls6yypu.appsync-api.us-east-1.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_appsync_apiKey": "da2-hpc7oihrvfekzbyp2yee2w3fku",
-    "aws_content_delivery_bucket": "projects-20190910123545-hostingbucket-devb",
+    "aws_content_delivery_bucket": "projects-20190910123545-hostingbucket-devc",
     "aws_content_delivery_bucket_region": "us-east-1",
-    "aws_content_delivery_url": "http://projects-20190910123545-hostingbucket-devb.s3-website-us-east-1.amazonaws.com",
+    "aws_content_delivery_url": "http://projects-20190910123545-hostingbucket-devc.s3-website-us-east-1.amazonaws.com",
+    "aws_cognito_identity_pool_id": "us-east-1:d01021f2-33c5-4c81-8135-45f5886baa9e",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_6xSztfbfz",
     "aws_user_pools_web_client_id": "5qq4a0gbtc87ce70v4r9dqi56j",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://ougo52ihunawhlxdf2qls6yypu.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
